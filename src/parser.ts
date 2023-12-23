6178a96f55980706ed14e719730a30a94d521803
Ryan Hmd <ryanhmd.pro@gmail.com>
2023-12-23 01:10:40 +0100

    Add support for more reference format

--- src/parser.ts
+++ src/parser.ts
@@ -7,17 +7,48 @@ const { bookTag } = require('bible-abbreviation');
  * ```
 */
 export const parseQuery = (ref: string) : BibleQuery => {
-    const splitter = new RegExp(/((?:\d\s)?[A-Za-zÀ-ÿ]+)\s(\d+)(?::(\d+[a-zA-Z]?(?:-\d+[a-zA-Z]?)?))?/);
+    const splitter = new RegExp(/((?:\d )?[A-Za-zÀ-ÿ]+) (\d+)(?::((?:\d+[a-zA-Z]?)(?:(?:-\d+[a-zA-Z]?)|(?:,\d+[a-zA-Z]?)+){0,1}))?/);
     if (!splitter.test(ref)) throw {code: 400, message: 'No valid reference to match'};
     const [, book, chapter, indexs] = ref.match(splitter)!;
+
     const tag = bookTag(book);
     if (!tag) throw {code: 404, message: "No book founded for the given reference"};
-    const [startIndex, stopIndex] = indexs ? indexs.split('-').map(x => x.toUpperCase()) : [null, null];
-    if (startIndex && startIndex === stopIndex) throw {code: 400, message: "Invalid reference"};
-    return {
-        book: tag,
-        chapter: chapter,
-        startIndex: startIndex,
-        stopIndex: stopIndex || null
-    };
+
+    if (!indexs) {
+        return {
+            book: tag,
+            chapter: chapter,
+            type: "WHOLE_CHAPTER"
+        };
+    }
+    else {
+        if (indexs.includes("-")) {
+            const [startIndex, stopIndex] = indexs.split('-').map(x => x.toUpperCase());
+            if (startIndex === stopIndex) throw {code: 400, message: "Nonsense range"};
+            return {
+                book: tag,
+                chapter: chapter,
+                type: "RANGE",
+                verses: [startIndex, stopIndex]
+            };
+        }
+        else if (indexs.includes(",")) {
+            const versesMap = indexs.split(',').map(x => x.toUpperCase());
+            const reducedMap = Array.from(new Set(versesMap));
+            return {
+                book: tag,
+                chapter: chapter,
+                type: "MAP",
+                verses: reducedMap
+            };
+        }
+        else {
+            return {
+                book: tag,
+                chapter: chapter,
+                type: "MAP",
+                verses: [indexs]
+            };
+        }
+    }
 }

185f0545aea69ea538087df258d72164b9114171
Ryan Hmd <ryanhmd.pro@gmail.com>
2023-12-28 10:23:22 +0100

    More concise conditions for indexs case

--- src/parser.ts
+++ src/parser.ts
@@ -14,41 +14,34 @@ export const parseQuery = (ref: string) : BibleQuery => {
     const tag = bookTag(book);
     if (!tag) throw {code: 404, message: "No book founded for the given reference"};
 
-    if (!indexs) {
+    if (!indexs) return { book: tag, chapter: chapter, type: "WHOLE_CHAPTER" };
+    
+    if (indexs.includes("-")) {
+        const [startIndex, stopIndex] = indexs.split('-').map(x => x.toUpperCase());
+        if (startIndex === stopIndex) throw {code: 400, message: "Nonsense range"};
         return {
             book: tag,
             chapter: chapter,
-            type: "WHOLE_CHAPTER"
+            type: "RANGE",
+            verses: [startIndex, stopIndex]
+        };
+    }
+    else if (indexs.includes(",")) {
+        const versesMap = indexs.split(',').map(x => x.toUpperCase());
+        const reducedMap = Array.from(new Set(versesMap));
+        return {
+            book: tag,
+            chapter: chapter,
+            type: "MAP",
+            verses: reducedMap
         };
     }
     else {
-        if (indexs.includes("-")) {
-            const [startIndex, stopIndex] = indexs.split('-').map(x => x.toUpperCase());
-            if (startIndex === stopIndex) throw {code: 400, message: "Nonsense range"};
-            return {
-                book: tag,
-                chapter: chapter,
-                type: "RANGE",
-                verses: [startIndex, stopIndex]
-            };
-        }
-        else if (indexs.includes(",")) {
-            const versesMap = indexs.split(',').map(x => x.toUpperCase());
-            const reducedMap = Array.from(new Set(versesMap));
-            return {
-                book: tag,
-                chapter: chapter,
-                type: "MAP",
-                verses: reducedMap
-            };
-        }
-        else {
-            return {
-                book: tag,
-                chapter: chapter,
-                type: "MAP",
-                verses: [indexs]
-            };
-        }
+        return {
+            book: tag,
+            chapter: chapter,
+            type: "MAP",
+            verses: [indexs]
+        };
     }
 }
